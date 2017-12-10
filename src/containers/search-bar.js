1027015d09fd4a297eee760d6cc95ff3a22d446b
TalhaTZA <talhavjtza@gmail.com>
2017-12-10 15:45:44 +0500

    ADD callback and fixed context bug

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -2,15 +2,33 @@ import React,{Component} from 'react';
 
 class SearchBar extends Component{
 
+    constructor(props){
+        super(props);
+        this.state={
+            term: ''
+        };
+
+        this.onInputChange=this.onInputChange.bind(this);
+    }
+
     render(){
         return(
             <form className='input-group'>
-                <input />
+                <input  
+                    placeholder='Get a five day forecast'
+                    className='form-control'
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className='input-group-btn'>
                     <button className='btn btn-secondary'>Submit</button>
                 </span>
             </form>
         );
+    }
+
+    onInputChange(event){
+        this.setState({term: event.target.value})
     }
 }
 
