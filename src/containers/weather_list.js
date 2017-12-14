fb5117c89806cb9cf5766f389dc09bb420bc6f82
TalhaTZA <talhavjtza@gmail.com>
2017-12-14 21:27:06 +0500

    ADD list fixed bugs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,40 @@
-import React , {Componet} from 'react';
+import React , {Component} from 'react';
+import {connect} from 'react-redux';
 
-class WeatherList extends Componet{
+class WeatherList extends Component{
     render(){
         return (
             <table className='table table-hover'>
                 <thead>
                     <tr>
-                        <th>
-                            City
-                        </th>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
             </table>
+        );
+    }
+
+    renderWeather(cityData){
+        
+        const name=cityData.city.name;
+        return(
+            <tr key={name}>
+                <td>{name}</td>
+            </tr>
         );
     }
 }
 
-export default WeatherList;
+function mapStateToProps({weather}){
+    return {
+        weather  
+    };
+}
+
+export default connect(mapStateToProps)(WeatherList);
